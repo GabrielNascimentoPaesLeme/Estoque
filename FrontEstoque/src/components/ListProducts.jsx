e2efd692c6378eca0a0bbabe4a6213871a69789d
Gabriel Paes Leme <gabrielnascimentopaesleme@gmail.com>
2024-11-19 11:09:59 -0300

    Atualização na exibição da lista de produtos

--- FrontEstoque/src/components/ListProducts.jsx
+++ FrontEstoque/src/components/ListProducts.jsx
@@ -3,9 +3,40 @@ import Accordion from 'react-bootstrap/Accordion';
 
 const ListProducts = ({ products }) => {
   const [isEditing, setIsEditing] = useState(false)
-  const handleNewQuantidy = (e) => [
-    console.log(e.target.value)
-  ]
+  const [updateProduct, setUpdateProduct] = useState([{
+    ref: '',
+    cor: '',
+    size: '',
+    quantidade: 0,
+  }])
+  const handleNewQuantidy = (e, size, cor, ref) => {
+    const existingUpdate = updateProduct.find(
+      (update) => update.cor === cor && update.size === size && update.ref === ref
+    )
+    if(!existingUpdate){
+      setUpdateProduct([
+        ...updateProduct,
+        {
+          ref: ref,
+          cor: cor,
+          size: size,
+          quantidade: e.target.value
+        }
+      ])
+    } else {
+      const handleUpdate = updateProduct.map((update)=> {
+        if (update.cor === cor && update.size === size && update.ref === ref) {
+          return {
+            ...update,
+            quantidade: e.target.value
+          }
+        }
+        return update
+      })
+      setUpdateProduct(handleUpdate)
+    }
+  }
+  console.log(updateProduct)
   return (
     <div>
       {products.map((product, index) => {
@@ -52,7 +83,7 @@ const ListProducts = ({ products }) => {
                             {sizes.map((size, sizeIndex) => (
                               <tr key={sizeIndex}>
                                 <td>{size.size}</td>
-                                {!isEditing ?<td>{size.quantidade}</td>:<td style={{width: '5%', padding: '0px'}}><input type="number" defaultValue={size.quantidade} style={{width: '50%', fontSize: '.95rem'}} onChange={(e) => handleNewQuantidy(e)} /></td>}
+                                {!isEditing ?<td>{size.quantidade}</td>:<td style={{width: '5%', padding: '0px'}}><input type="number" defaultValue={size.quantidade} style={{width: '50%', fontSize: '.95rem'}} onChange={(e) => handleNewQuantidy(e, size.size, size.cor, product.ref)} /></td>}
                                 
                               </tr>
                             ))}
